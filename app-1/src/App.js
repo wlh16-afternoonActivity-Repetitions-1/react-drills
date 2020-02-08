2640d0a158967ab8c9172f3f4b4a55ab66421de5
Jacob Hall <halljaco@yahoo.com>
2020-02-08 11:39:15 -0700

    ReactDrillApp1_repeat4

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,26 +1,183 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      message: ""      
+    }
+
+  }
+
+  handleChange(liveInput){
+    this.setState({message: liveInput})
+  }
+
+  render(){
+    return (
+      <div>
+        <input onChange={e => this.handleChange(e.target.value)} />
+        <p>{this.state.message}</p>
+
+      </div>
+    )
+  }
+
 }
 
 export default App;
+
+
+
+
+
+
+// import React, {Component} from 'react'
+
+// class App extends Component{
+//   constructor(props){
+//     super(props)
+
+//     this.state = {
+//       message: ""
+
+//     }
+//   }
+
+//   handleChange(valueInput){
+//     this.setState({message: valueInput})
+//   }
+
+//   render(){
+//     return(
+//       <div>
+//         <input onChange = {e=> this.handleChange(e.target.value)} />
+//         <p>{this.state.message}</p>
+//       </div>
+//     )
+//   }
+
+// }
+
+// export default App
+
+
+
+
+
+// import React, {Component} from 'react'
+
+// class App extends Component {
+//   constructor (props) {
+//   super (props)
+
+//   this.state = {
+//     message: ""
+//   };
+  
+//   }
+
+//   handleChange(value){
+//     console.log(value)
+//     this.setState({message: value});
+//   }
+
+//   render() {
+//     console.log(this.state)
+//     return (
+//       <div className = "App">
+//         <input onChange = {(e) => this.handleChange(e.target.value) } type = "text" />
+//         <p>{this.state.message}</p>
+//       </div>
+//     );
+//   }
+
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// import './App.css';
+
+// class App extends Component{
+//   constructor(){
+//     super()
+
+//     this.state = {
+//       message: ""
+//     };
+//   };
+
+
+//   handleChange(value){
+//     this.setState({message: value})
+//   }
+
+
+//   render(){
+//     return (
+//       <div>
+//         <input onChange={ (e) => this.handleChange(e.target.value) }></input>
+
+//       </div>
+//     )
+//   }
+
+
+// }
+
+// export default App;
