245257afb00a2c180e229f82333eedd3b73326fb
Jacob Hall <halljaco@yahoo.com>
2020-02-09 13:40:12 -0700

    ReactDrill-App3 initial attempt partialy complete

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+
+      foods: ['cheese', 'crackers', 'pringles', 'oranges', 'potatoes', 'bacon', 'avocado']
+
+    }
+  }
+
+
+
+  render(){
+
+    let foodsDisplayed = this.state.foods.map((element,index) => {
+      return <h2 key = {index} >{element}</h2>
+    })
+
+
+    return(
+      <div>
+        <input  />
+        {foodsDisplayed}
+      </div>
+    )
+
+  }
+
+
 }
-
-export default App;
+export default App
