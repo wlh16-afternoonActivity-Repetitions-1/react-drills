5abbb8cb3654bc4b3490976c58874bef4f6e9c7e
Jacob Hall <halljaco@yahoo.com>
2020-02-08 17:31:18 -0700

    ReactDrills-App2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,40 +1,240 @@
 import React, {Component} from 'react';
 
-
 class App extends Component {
   constructor(props){
-    super(props)
+    super(props);
+
 
     this.state = {
-
-      soda: ['cola', 'sprite','Mellow Yellow', 'Iron Port', 'Apple Beer']
-    }
-
+      stuff: ["true", "false", "true", "true"]
+    };
   }
 
-  // handleChange(value){
-  //   this.setState({data: value})
-  // }
+  render(){
 
-
-  render() {
-
-    let sodaToDisplay = this.state.soda.map((element,index) => {
-      return <h2 key={index}>{element}</h2>
+    let newStuff = this.state.stuff.map((element,index) => {
+      return <h2>{element}</h2>
     })
-
-
+    
+    
     return (
-      <div>
-
-      {sodaToDisplay}
-
-      </div>
+    <div> 
+      {newStuff}
+    </div>
     )
-
   }
-
 
 }
 
 export default App
+
+
+// import React, {Component} from 'react'
+
+// class App extends Component {
+//   constructor(props){
+//     super(props)
+
+//     this.state = {
+//       typedInfo: ""
+//     }
+//   }
+
+//   handleChange(newValues){
+//     this.setState({typedInfo: newValues})
+//   }
+
+//   render(){
+
+//       return (
+//         <div>
+//           <input  onChange = {(e) => this.handleChange(e.target.value)}  />
+//           <p>{this.state.typedInfo}</p>
+//         </div>
+//       )
+
+//   }
+
+// }
+
+// export default App;
+
+// import React, {Component} from 'react'
+
+// class App extends Component {
+//   constructor(props){
+//     super(props);
+
+//     this.state = {
+//       textInput: ""
+//     };
+//   }
+
+//   handleChange(newInput){
+//       this.setState({textInput: newInput});
+//   }
+    
+    
+//     render() {
+//       return (
+//         <div>
+//         <input onChange ={ e => this.handleChange(e.target.value)}/>
+//         <p>{this.state.textInput}</p>
+//       </div>
+//     );
+//   }  
+// }
+
+// export default  App;
+
+
+
+
+// import React, {Component} from 'react';
+
+// class App extends Component{
+//   constructor(props){
+//     super(props)
+
+//     this.state = {
+//       numbers: [1,2,3,4,5,6,5,4,3,2,1]
+//     };
+//   }
+
+
+//   render(){
+    
+//     let numbersToDisplay = this.state.numbers.map((element,index) => {
+//       return <h2 key={index}>{element}</h2>;
+//     });
+    
+    
+//     return(
+//       <div>
+//         {numbersToDisplay}  
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
+
+
+
+
+// import React, {Component} from 'react'
+
+// class App extends Component {
+//   constructor(props){
+//     super()
+
+//     this.state = {
+//       names: ["John","Jacob", 'Jingle', 'Hiemer', 'Smitt']
+//     }
+//     //console.log(this.state.names)
+//   }
+
+//   render(){
+
+//     let namesToDisplay = this.state.names.map((element, index) => {
+//         return <h2 key = {index}>{element}</h2>;
+//       });
+
+    
+
+      
+    
+    
+    
+//     return(
+//       <div>
+//           {namesToDisplay}
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
+
+
+
+
+// import React, {Component} from 'react'
+
+// class App extends Component {
+//   constructor(props){
+//     super(props)
+
+//     this.state = {
+//       arr: ['Chicago', 'Seattle', 'San Francisco', 'Baltimore', 'Boise', 'Houston']
+//     }
+//   }
+
+//   render(){
+    
+//     let arrToDisplay = this.state.arr.map((element, index) => {
+//       return <h2 key = {index}>{element}</h2>
+//     });
+
+//     return (
+//       <div>
+//         {arrToDisplay}
+//       </div>
+
+//     );
+//   }
+
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+// import React, {Component} from 'react';
+
+
+// class App extends Component {
+//   constructor(props){
+//     super(props)
+
+//     this.state = {
+
+//       soda: ['cola', 'sprite','Mellow Yellow', 'Iron Port', 'Apple Beer']
+//     }
+
+//   }
+
+//   // handleChange(value){
+//   //   this.setState({data: value})
+//   // }
+
+
+//   render() {
+
+//     let sodaToDisplay = this.state.soda.map((element,index) => {
+//       return <h2>{element}</h2>; // ask about this line <--------------------------------------------------
+//     })
+
+
+//     return (
+//       <div>
+
+//       {sodaToDisplay}
+
+//       </div>
+//     )
+
+//   }
+
+
+// }
+
+// export default App
