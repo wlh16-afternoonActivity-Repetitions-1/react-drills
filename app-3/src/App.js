240a62a8d7ba9d7c0ee2c9c3fc9c72ee01d8cbb4
Jacob Hall <halljaco@yahoo.com>
2020-02-09 19:50:31 -0700

    ReactDrills-app3 complete

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -6,23 +6,41 @@ class App extends Component {
 
     this.state = {
 
+      filterString: "",
+
       foods: ['cheese', 'crackers', 'pringles', 'oranges', 'potatoes', 'bacon', 'avocado']
 
     }
   }
 
 
+  handleChange(filter) {
+    this.setState({filterString: filter})
+  }
+
+
 
   render(){
 
-    let foodsDisplayed = this.state.foods.map((element,index) => {
-      return <h2 key = {index} >{element}</h2>
+    // let foodsDisplayed = this.state.foods.map((element,index) => {
+    //   return <h2 key = {index} >{element}</h2>
+    // })
+
+    let foodsDisplayed = this.state.foods.filter((element,index) => {
+      return element.includes(this.state.filterString);
     })
+
+    .map ((element,index) => {
+    return <h2 key={index}>{element}</h2>
+    })
+
+
+
 
 
     return(
       <div>
-        <input  />
+        <input  onChange = {e => this.handleChange(e.target.value) }  />
         {foodsDisplayed}
       </div>
     )
